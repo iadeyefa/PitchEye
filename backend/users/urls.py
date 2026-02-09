1ea14fd4fc97b2daf6616e1162419a6832a81f73
bluebird-ot <64104983+bluebird-ot@users.noreply.github.com>
2026-02-09 16:04:55 -0500

    added get user, user/profiles schema

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.list_users),
+    path('',views.get_user),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -5,6 +5,16 @@ from rest_framework.response import Response
 
 @api_view(['GET'])
 def list_users(request):
-    data = supabase.table('users').select('*').execute()
+    data = supabase.table('profiles').select('*').execute()
     return Response(data.data)
 
+@api_view(['GET'])
+def get_user(request, user_id): 
+    data = supabase.table('profiles').select('*').eq('id', user_id).execute()
+    return Response(data.data)
+
+"""
+TODO: 
+- Change profile information
+- Delete user
+"""

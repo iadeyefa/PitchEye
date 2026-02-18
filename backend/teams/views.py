a739c0e1907cb0e90d8875dd3efbb4c6fa3353cc
bluebird-ot <64104983+bluebird-ot@users.noreply.github.com>
2026-02-18 12:07:06 -0500

    added join code and fixed get_team api

--- backend/teams/views.py
+++ backend/teams/views.py
@@ -1,3 +1,5 @@
+import random
+import string
 from django.shortcuts import render
 from utils.supabase_client import supabase
 from rest_framework.decorators import api_view
@@ -11,7 +13,7 @@ def list_teams(request):
 @api_view(['GET'])
 def get_team(request):
     team_code = request.query_params.get('code')
-    data = supabase.table('teams').select('*').eq('id', team_code).execute()
+    data = supabase.table('teams').select('*').eq('join_code', team_code).execute()
     return Response(data.data)
 
 @api_view(['POST'])
@@ -19,9 +21,10 @@ def create_team(request):
     team_name = request.data.get('teamName')
     admin_id = request.data.get('adminID')
 
-    # create team in teams table
-    data = supabase.table('teams').insert({'name': team_name, 'admin_id': admin_id}).execute()
     # generate some random code for team_code in teams
+    join_code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+    # create team in teams table
+    data = supabase.table('teams').insert({'name': team_name,'join_code': join_code ,'admin_id': admin_id}).execute()
 
     # update team_id var in the admin_id with the new team_id
     new_team_id = data.data[0]['id']

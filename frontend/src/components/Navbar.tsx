60147cbdaf7cfe21ec150abe51082e0526125986
bluebird-ot <64104983+bluebird-ot@users.noreply.github.com>
2026-02-18 13:57:24 -0500

    wip nav bar switched between two styling for mobile/desktop, need to test for mobile breakpoints

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,23 +1,23 @@
-import { use, useState } from "react"
 import { Link } from "react-router-dom"
 import '../styles/Navbar.css'
 
 export default function Navbar() {
-    const [isOpen, setIsOpen] = useState(false);
     return (
-        <nav className="navbar">
-            <button type="button" title='hamburger' className="hamburger" onClick={() => setIsOpen(!isOpen)}>
-                <span/>
-                <span/>
-                <span/>
-                <span/>
-            </button>
-            <ul className={`nav-links ${isOpen ? "open" : ""}`}>
-                <li><Link to="/" onClick={() => setIsOpen(false)}>Home</Link></li>
-                <li><Link to="/live" onClick={() => setIsOpen(false)}>Live</Link></li>
-                <li><Link to="/upload" onClick={() => setIsOpen(false)}>Upload</Link></li>
-                <li><Link to="/profile" onClick={() => setIsOpen(false)}>Profile</Link></li>
+        <>
+        <nav className="desktop-nav">
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/live">Live</Link></li>
+                <li><Link to="/upload">Upload</Link></li>
+                <li><Link to="/profile">Profile</Link></li>
             </ul>
         </nav>
+        <nav className="mobile-nav">
+                <Link to="/">Home</Link>
+                <Link to="/live">Live</Link>
+                <Link to="/upload">Upload</Link>
+                <Link to="/profile">Profile</Link>
+        </nav>
+        </>
     )
 }

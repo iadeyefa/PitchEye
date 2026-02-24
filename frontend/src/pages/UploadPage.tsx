145184ce650161f6c74ef141ade77574740f263a
bluebird-ot <64104983+bluebird-ot@users.noreply.github.com>
2026-02-24 16:02:32 -0500

    WIP starting upload page

--- frontend/src/pages/UploadPage.tsx
+++ frontend/src/pages/UploadPage.tsx
@@ -1,3 +1,13 @@
+import { useState } from "react";
+
 export default function Upload() {
+  const [video, setVideo] = useState(null);
+  const [videoURL, setVideoURL] = useState('');
+  
+  const [caption, setCaption] = useState('');
+  const [currentTag, setCurrentTag] = useState('');
+  
+
+
   return <div>Upload goes here</div>
 }
